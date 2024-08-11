67920268d4556c95a2f12457f4bac602de029143
Ang Kok Leong <ang.kok.leong.work@gmail.com>
2024-08-11 14:57:13 +0000

    modified the folder name to etl_processor, added another line to modify the module_file_path in the configuration text file, added two variable for initializing the CSVFileExtract class and ExcelFileExtract class

--- etl_processor/__init__.py
+++ etl_processor/__init__.py
@@ -1,14 +1,18 @@
 import configparser
-import data_processor.etl
-import data_processor.describe
-import data_processor.analysis
+import etl_processor.extract
+
 import os
 
 
-file_path: str = os.path.abspath(os.path.relpath("etl-processor"))
+file_path: str = os.path.abspath(os.path.relpath("etl_processor"))
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
 
 config.read(file_path + '/configuration/csv_file_path_configuration.txt')
 
 config.set("DEFAULT", "module_file_path", file_path)
+
+
+csv_file_data_source = etl_processor.extract.CSVFileExtract(config)
+excel_file_data_source = etl_processor.extract.ExcelFileExtract(config)
+
